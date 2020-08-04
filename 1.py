1b2bad7f2124cf0cccfc7ba4ef4f796b0c9edd36
go0space <gozjd999@gmail.com>
2020-08-04 16:30:27 +0900

    1.py

---
+++ 1.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+c = a + b
+print(c) # 3
+
+x = 1
+if x > 0:
+    a = 1
+    b = 2
+    c = a + b
+else:
+    a = -1
+    b = -2
+    c = a - b    
+
+print(c) # 3   
+
+import math
+
+n = math.sqrt(9.0) # sqrt 함수 숫자 x의 제곱근 반환
+
+print(n)  #3.0 출력
+
---
+++ 2.py
@@ -0,0 +1,10 @@
+int(3.5) # 3
+2e3 # = 2* (10**3) = 2000.0
+float(1.6) # 1.6
+float("inf") # 무한대
+float("-inf") # -무한대
+bool(0) # False. 숫자에서 0만 False
+bool(-1) # True
+bool("False") # True
+a = None # a는 None
+a is None # a가 None 이므로 True
